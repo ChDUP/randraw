f207e63e1d47e552a8e369768d263f5f3168fea6
Laurent <mail@chdup.fr>
2022-11-30 09:04:02 +0100

    add tailwind conf

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./assets/**/*.{svelte,js,ts,tsx}",
+    "./templates/**/*.{html,twig}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
