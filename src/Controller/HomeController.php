b84fbfae175dc34a0132560e09893f551cb40694
Laurent <mail@chdup.fr>
2022-11-30 08:35:07 +0100

    change routing and add show figure page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
+use CredentialType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(): Response
-    {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
-    }
+
 }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,17 +2,64 @@
 
 namespace App\Controller;
 
+use App\Form\CredentialType;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
-    public function index(): Response
+    #[Route('/', name: 'app_home')]
+    public function index(Request $request, UserRepository $userRepository): Response
     {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+        $error = false;
+        $form = $this->createForm(CredentialType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $form_data = $form->getData();
+            $credential = $form_data['credential'];
+
+            if($user = $userRepository->findOneBy([
+                'credential' => $credential
+                ], [])) {
+
+                dd($user->getAssigned());
+                if ($user->getAssigned() == true) {
+                    return $this->render('figure/selected.html.twig', [
+                            'user' => $user,
+                            'figure' => $user->getFigure()
+                        ]
+                    );
+                } else {
+                    return $this->render('figure/shuffle.html.twig', [
+                            'user' => $user,
+                            'figure' => $user->getFigure()
+                        ]
+                    );
+                }
+            } else {
+                $error = true;
+            }
+
+
+//            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->renderForm('home/index.html.twig', [
+            'credential_form' => $form,
+            'controller_name' => 'HomeController',
+            'error' => $error
         ]);
     }
+
+//    #[Route('/user', name: 'app_user')]
+//    public function index(): Response
+//    {
+//        return $this->render('user/index.html.twig', [
+//            'controller_name' => 'UserController',
+//        ]);
+//    }
 }
